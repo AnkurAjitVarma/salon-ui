d57676609e538783ac0f23f126dcbffccc6a0a64
AnkurAjitVarma <ankur.a.varma@outlook.com>
2025-11-20 14:33:37 -0600

    Initial Commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,9 @@
+import { createRootRoute } from "@tanstack/react-router";
+import Root from "../components/Root.tsx";
+
+
+const root = createRootRoute({
+    component: Root
+})
+
+export { root as Route }
